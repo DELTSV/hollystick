2035eae4684447a5a7cfe65ae9729aa16f2bebfe
MacaronFR <denisft77@gmail.com>
2024-06-24 16:41:43 +0200

    Exo 2

---
+++ exo2/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
